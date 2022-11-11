5440dd5d838eeb1d1b41826260e9951b660281d5
ivan-pl <pleskaiv@gmail.com>
2022-11-11 10:41:50 +0300

    build: add requirements.txt

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from gamemodel import GameModel
 
 if __name__ == "__main__":
     gameView = GameView()
-    gameModel = GameModel(1, 1)
+    gameModel = GameModel(3, 9)
     gameController = GameController(gameView, gameModel)
     gameController.start_game()
