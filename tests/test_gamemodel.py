c88e82584821ec2e9227cbc7e86cae7eea80709a
ivan-pl <pleskaiv@gmail.com>
2022-11-22 17:29:27 +0300

    feat: add tests for gamemodel

--- tests/test_gamemodel.py
+++ tests/test_gamemodel.py
@@ -4,22 +4,37 @@ from gamemodel import GameModel, Card
 
 @fixture
 def game_model_inst(request):
-    (rows, columns) = request.param
-    game_model = GameModel(rows, columns)
+    game_model = GameModel(2, 3)
     game_model.new_game()
     return game_model
 
 
+@fixture
+def card_player_inst():
+    arr2d = [[1, 2, 3], [4, 5, 6]]
+    card = Card(len(arr2d), len(arr2d[0]))
+    card.card = arr2d
+    return card
+
+
+@fixture
+def card_pc_inst():
+    arr2d = [[11, 12, 13], [14, 15, 16]]
+    card = Card(len(arr2d), len(arr2d[0]))
+    card.card = arr2d
+    return card
+
+
 class TestModel:
 
-    @mark.parametrize("game_model_inst", [(2, 3)], indirect=True)
+    @mark.parametrize("game_model_inst", [...], indirect=True)
     def test_new_game(self, game_model_inst):
         assert isinstance(game_model_inst.player_card, Card)
         assert isinstance(game_model_inst.pc_card, Card)
         assert game_model_inst.player_card.rows == 2
         assert game_model_inst.player_card.columns == 3
 
-    @mark.parametrize("game_model_inst", [(2, 3)], indirect=True)
+    @mark.parametrize("game_model_inst", [...], indirect=True)
     def test_next_keg(self, game_model_inst):
         prev_kegs_left = 101
         for i in range(10):
@@ -33,3 +48,14 @@ class TestModel:
         nums_set = set(num for (num, _) in GameModel._create_keg_generator())
         assert len(nums_set) == 90
         assert all([type(num) is int for num in nums_set])
+
+    @mark.parametrize("game_model_inst, card_player_inst, card_pc_inst, cur_keg, answer, expected_val", [
+        param(..., ..., ..., 1, 'n', (True, "Игрок ошибся"), id="Player makes mistake"),
+        param(..., ..., ..., 1, 'y', (False, ""), id="Game continues"),
+    ], indirect=["game_model_inst", "card_player_inst", "card_pc_inst"])
+    def test_next_turn(self, game_model_inst, card_player_inst, card_pc_inst, cur_keg, answer, expected_val):
+        game_model_inst.current_keg = cur_keg
+        game_model_inst.player_card = card_player_inst
+        game_model_inst.pc_card = card_pc_inst
+        actual_answer = game_model_inst.next_turn(answer)
+        assert actual_answer == expected_val
